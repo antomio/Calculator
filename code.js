673bf5c4b7b930c202925b475b12d23a421f5f97
Tomsantomio <toms.pirktins@gmail.com>
2016-03-20 18:33:59 +0200

    FINISH

--- code.js
+++ code.js
@@ -1,56 +1,58 @@
 function resetCalculator(curValue){
-	$(".display").val(curValue);
+	$("#d").val(curValue);
 	$(".function").removeClass("pendingFunction");
-	$(".display").data("isPendingFunction", false); //tiiri css'am
-	$(".display").data("thePendingFunction", "");
-	$(".display").data("valueOneLocked", false);
-	$(".display").data("valueTwoLocked", false);
-	$(".display").data("valueOne", curValue);
-	$(".display").data("valueTwo", 0);
-	$(".display").data("fromPrevious", false);
+	$("#d").data("isPendingFunction", false); //tiiri css'am
+	$("#d").data("thePendingFunction", "");
+	$("#d").data("valueOneLocked", false);
+	$("#d").data("valueTwoLocked", false);
+	$("#d").data("valueOne", curValue);
+	$("#d").data("valueTwo", 0);
+	$("#d").data("fromPrevious", false);
 }
 
 (function($) {
 
 	// $('.keys input[type="button"]').click(function(){
-	//     $('.display input[type="text"]').val($(this).val());
+	//     $('#d input[type="text"]').val($(this).val());
 	// });
 
+	resetCalculator("0");
+
 	$(".num").click(function() {
-		if ($(".display").data("fromPrevious") == true){
+		if ($("#d").data("fromPrevious") == true){
 			resetCalculator($(this).text());
-		} else if (($(".display").data("isPendingFunction") == true) && 
-			($(".display").data("valueOneLocked") == false)){
-				$(".display").data("valueOne", $(".display").val());
-				$(".display").data("valueOneLocked", true);
-				$(".display").val($(this).text());
-				$(".display").data("valueTwo", $(".display").val());
-				$(".display").data("valueTwoLocked", true);
+		} else if (($("#d").data("isPendingFunction") == true) && 
+			($("#d").data("valueOneLocked") == false)){
+				$("#d").data("valueOne", $("#d").val());
+				$("#d").data("valueOneLocked", true);
+				$("#d").val($(this).text());
+				$("#d").data("valueTwo", $("#d").val());
+				$("#d").data("valueTwoLocked", true);
 
 // Clicking a number AGAIN, after first number is locked and already there's a value for the second number
 
-		} else if (($(".display").data("isPendingFunction") == true ) &&
-			($(".display").data("valueOneLocked") == true)){
+		} else if (($("#d").data("isPendingFunction") == true ) &&
+			($("#d").data("valueOneLocked") == true)){
 
-			var curValue = $(".display").val();
+			var curValue = $("#d").val();
 			var toAdd = $(this).text();
 			var newValue = curValue + toAdd;
 
-			$(".display").val(newValue);
-			$(".display").data("valueTwo", $(".display").val());
-			$(".display").data("valueTwoLocked", true);
+			$("#d").val(newValue);
+			$("#d").data("valueTwo", $("#d").val());
+			$("#d").data("valueTwoLocked", true);
 
 // Clicking on a fresh number
 	
 		} else {
-			var curValue = $(".display").val();
+			var curValue = $("#d").val();
 			if (curValue == "0") {
 				curValue = "";
 			}
 			var toAdd = $(this).text();
 			var newValue = curValue + toAdd;
 
-			$(".display").val(newValue);
+			$("#d").val(newValue);
 		}
 	});
 
@@ -59,30 +61,42 @@ function resetCalculator(curValue){
 	});
 
 	$(".equals").click(function() {
-		if (($(".display").data("valueOneLocked") == true) &&
-			($(".display").data("valueTwoLocked") == true)){
+		if (($("#d").data("valueOneLocked") == true) &&
+			($("#d").data("valueTwoLocked") == true)){
 
-			if ($(".display").data("thePendingFunction") == ""){
-				var finalValue = parseFloat($(".display").data("valueOne")) + 
-				parseFloat($(".display").data("valueTwo"));
-			} else if ($(".display").data("thePendingFunction") + 
-				parseFloat($(".display").data("valueTwo")))
+			if ($("#d").data("thePendingFunction") == "+"){
+				var finalValue = parseFloat($("#d").data("valueOne")) + 
+				parseFloat($("#d").data("valueTwo"));
+			} else if ($("#d").data("thePendingFunction") == "-") {
+				var finalValue = parseFloat($("#d").data("valueOne")) - 
+				parseFloat($("#d").data("valueTwo"));
+			} else if ($("#d").data("thePendingFunction") == "*") {
+				var finalValue = parseFloat($("#d").data("valueOne")) * 
+				parseFloat($("#d").data("valueTwo"));
+			} else if ($("#d").data("thePendingFunction") == "/") {
+				var finalValue = parseFloat($("#d").data("valueOne")) / 
+				parseFloat($("#d").data("valueTwo"));
+			}
 
-				var finalValue = parseFloat($(".display").data("valueOne")) + 
-				parseFloat($(".display").data("valueTwo"));
+			$("#d").val(finalValue);
+
+			resetCalculator(finalValue);
+			$("#d").data("fromPrevious", true);
+		} else {
+			//both numbers are NOT locked. do nothing.
 		}
 	});
 
 	$(".function").click(function() {
-		if ($(".display").data("fromPrevious") == true){
-			resetCalculator($("".display).val());
-			$(".display").data("valueOneLocked", false);
-			$(".display").data("fromPrevious", false);
+		if ($("#d").data("fromPrevious") == true){
+			resetCalculator($("#d").val());
+			$("#d").data("valueOneLocked", false);
+			$("#d").data("fromPrevious", false);
 		}
 // Let it be known that a function has been selected
 		var pendingFunction = $(this).text();
-		$(".display").data("isPendingFunction", true);
-		$(".display").data("thePendingFunction", pendingFunction);
+		$("#d").data("isPendingFunction", true);
+		$("#d").data("thePendingFunction", pendingFunction);
 // Visually represent the current function
 		$(".function").removeClass("pendingFunction");
 		$(this).addClass("pendingFunction");
